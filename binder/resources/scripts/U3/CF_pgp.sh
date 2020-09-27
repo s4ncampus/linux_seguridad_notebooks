2101539277cbbb40442b1589a9338406fa1c20f9
alejokalderon@gmail.com <L0v33l3ktronixx*g>
2020-09-26 22:58:21 -0500

    Testing gpg

--- binder/resources/scripts/U3/CF_pgp.sh
+++ binder/resources/scripts/U3/CF_pgp.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-randomfilename=$(sed "s/[^a-zA-Z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1))
+#randomfilename=$(sed "s/[^a-zA-Z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1))
+randomfilename=$(shuf -i 2000-65000 -n 1)
+echo $randomfilename
 echo "Introduzca a continuación el correo con el que creó su par de llaves:"
 read email
 if echo "${email}" | grep '^[a-zA-Z0-9]*@[a-zA-Z0-9]*\.[a-zA-Z0-9]*$' >/dev/null; then
         touch /tmp/${randomfilename}
         echo "Hey, what you doin?" > /tmp/${randomfilename}
-if gpg --output ./archivo-cifrado.pgp --encrypt --recipient ${email} /tmp/${randomfilename} --trust-model always 2> /dev/null; then
+if gpg --output ./archivo-cifrado.pgp --encrypt --recipient ${email} /tmp/${randomfilename} 2> /dev/null; then
         echo ""
         echo "Se acaba de crear un archivo cifrado con la llave pública del email ingresado en este mismo directorio."
 	echo "El archivo creado se llama: \"archivo-cifrado.gpg\", deberás descifrarlo utilizando tu llave privada ya que allí está tu flag."
